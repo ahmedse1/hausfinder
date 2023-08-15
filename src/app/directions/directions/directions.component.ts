ee225943a79146653ff38ae4f71297f22f1053fa
Ahmed Hassan <Ahmed.Hassan@gls-itservices.com>
2023-08-15 12:48:21 +0200

    code optimization

--- src/app/directions/directions/directions.component.ts
+++ src/app/directions/directions/directions.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { MapboxService } from 'src/app/services/mapbox.service';
 
 @Component({
   selector: 'app-directions',
@@ -7,18 +8,73 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class DirectionsComponent implements OnInit {
 
-  @Input()
-  duration!: number;
-  @Input()
-  distance!: number;
-  @Input()
-  steps!: any;
+  duration: number;
 
+  distance: number;
 
-  constructor() { }
+  steps: any;
+
+  @Input()
+  map!: mapboxgl.Map;
+
+  end: [number, number]
+
+  constructor(private mapBoxService: MapboxService) {
+    this.duration = 0.1;
+    this.distance = 0;
+    this.end = [9.6842208, 50.5650941];
+  }
 
 
   ngOnInit() {
+
+  }
+
+  async getRoute(start: Number[], mode: string) {
+    debugger
+    const json = await this.mapBoxService.getRoute(start, this.end, mode);
+
+    const data = json.routes[0];
+    const route = data.geometry.coordinates;
+    const geojson: GeoJSON.Feature<GeoJSON.Geometry, GeoJSON.GeoJsonProperties> = {
+      type: "Feature",
+      properties: {},
+      geometry: {
+        type: "LineString",
+        coordinates: route,
+      },
+    };
+    // if the route already exists on the map, we'll reset it using setData
+    const source = this.map.getSource("route");
+    debugger;
+    if (source && typeof source === "object" && "setData" in source) {
+      source.setData(geojson);
+    }
+
+    // otherwise, we'll make a new request
+    else {
+      this.map.addLayer({
+        id: "route",
+        type: "line",
+        source: {
+          type: "geojson",
+          data: geojson,
+        },
+        layout: {
+          "line-join": "round",
+          "line-cap": "round",
+        },
+        paint: {
+          "line-color": "#3887be",
+          "line-width": 4,
+          "line-opacity": 0.75,
+        },
+      });
+    }
+
+    this.steps = data.legs[0].steps;
+    this.duration = Math.floor(data.duration / 60);
+    this.distance = Math.floor(data.distance / 1000);
 
   }
 }
